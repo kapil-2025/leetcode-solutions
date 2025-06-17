33182cb7b35271b8e0ebda54a7e4152eb97ab83e
Kapil Gupta <kapilguptakapil1111@gmail.com>
2025-06-17 10:20:15 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (99.22%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -4,6 +4,6 @@ class Solution {
         else if(n==1){return true;}
         else{while(n%2==0){n=n/2;} }
         if(n==1){return true;}
-        else {return false;}
+        else{return false;}
     }
 }
