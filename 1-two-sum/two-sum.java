e472c35b2c3d41a7f0bf48b4dd4a8dc410d874f3
Kapil Gupta <kapilguptakapil1111@gmail.com>
2025-06-19 03:41:14 -0700

    Time: 49 ms (11.84%) | Memory: 44.8 MB (78.40%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -5,7 +5,7 @@ class Solution {
             for(int j=i+1; j<nums.length; j++){
                 if(nums[i]+nums[j]==target){
                     ans[0]=i; ans[1]=j;
-                    break;
+                   
                 }
             }
         }return ans;
