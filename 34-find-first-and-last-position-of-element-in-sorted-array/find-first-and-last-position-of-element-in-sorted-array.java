26c78aafefe3d8bebc9304d1ffbebcb5b0f95d9a
Kapil Gupta <kapilguptakapil1111@gmail.com>
2025-06-20 06:43:34 -0700

    Time: 0 ms (100.00%) | Memory: 46 MB (48.07%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,40 +1,36 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int ans[]={-1,-1};
-    if(nums.length==0){
+
+        int [] ans={-1,-1};
+        if(nums.length==0){
+            return ans;
+        } ans[0]=Fo(nums,target);
+         ans[1]=so(nums,target);
         return ans;
-    }
-    ans[0]=firstOccurence(nums,target);
-    ans[1]=lastOccurence(nums,target);
-    return ans;
-    } public int firstOccurence(int [] nums , int target){
-        int s=0;
-        int e=nums.length-1;
-        int ans=-1;
-        while(s<=e){
-            int mid = (s+e)/2;
-            if(nums[mid]==target){
-ans=mid;
-e=mid-1;
-            }else if(target>nums[mid]){
-                s=mid+1;
-            }else{
-                e=mid-1;}} 
-                return ans;}
-                public int lastOccurence(int [] nums , int target){
-        int s=0;
-        int e=nums.length-1;
-        int ans=-1;
-        while(s<=e){
-            int mid = (s+e)/2;
-            if(nums[mid]==target){
-ans=mid; s=mid+1;
-            }else if(target>nums[mid]){
-                s=mid+1;
-            }else{
-                e=mid-1;
-            }
+       } public int so(int [] nums,int target){
+       int s=0; int ans=-1;
+       int e=nums.length-1;
+       
+       while(e>=s){int mid=(s+e)/2;
+        if(target==nums[mid]){
+            ans=mid;
+            s=mid+1;               
+        }else if(target>nums[mid]){
+            s=mid+1;
+        }else{
+            e=mid-1;}}
+            return ans;} 
+             public int Fo(int [] nums,int target){
+       int s=0; int ans=-1;
+       int e=nums.length-1;
+      
+       while(e>=s){int mid=(s+e)/2;
+        if(target==nums[mid]){
+            ans=mid; e=mid-1;               
+        }else if(target>nums[mid]){
+            s=mid+1;
+        }else{
+            e=mid-1;
         }
-return ans; 
-    }
+       }return ans;}
 }
