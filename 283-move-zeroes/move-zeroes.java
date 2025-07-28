72e6a408746d3654a01db33b28a1a087570afe2f
Kapil Gupta <kapilguptakapil1111@gmail.com>
2025-07-28 16:54:38 -0700

    Time: 2 ms (80.07%) | Memory: 46.7 MB (5.63%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -4,11 +4,11 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                if (i != lastNonZeroIndex) {
+                
                     int temp = nums[i];
                     nums[i] = nums[lastNonZeroIndex];
                     nums[lastNonZeroIndex] = temp;
-                }
+               
                 lastNonZeroIndex++;
             }
         }
