09669aa66d784458fb9fc73ac59bcc6ccf15cc59
Kapil Gupta <kapilguptakapil1111@gmail.com>
2025-07-27 18:12:37 -0700

    Time: 2 ms (98.50%) | Memory: 43 MB (76.49%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String clean="";
-        for(int i=0; i<=s.length()-1; i++){
-            char ch =s.charAt(i);
-            if(Character.isLetterOrDigit(ch)){
-clean=clean+Character.toLowerCase(ch);
+        int right=s.length()-1;
+        int left=0;
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            } while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
             }
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            right--;
+            left++;
         }
-        String reverse="";
-        for(int i=clean.length()-1; i>=0; i--){
-            reverse=reverse+clean.charAt(i);
-        }
-        return clean.equals(reverse);
+return true;
     }
 }
